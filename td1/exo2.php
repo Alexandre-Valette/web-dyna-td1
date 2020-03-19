94c3444f53ccdf48eb8ebfb0e651286548255098
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-19 11:44:26 +0100

    Add files via upload

--- td1/exo2.php
+++ td1/exo2.php
@@ -6,6 +6,7 @@
 </head>
 
 
+<body>
 <form method="GET">
     <label for="color">Color : </label>
     <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
@@ -13,19 +14,21 @@
     <input type="number" value="" name="size" id="size">
     <input type="submit" value="Valider">
 </form>
-<body>
-<a href="?Color=%23ff0000&size=15&message=Erreur 404">Message 1</a>
-<a href="?Color=%2339ff00&size=30&message=Erreur 404">Message 2</a>
-<a href="?Color=%230080ff&size=50&message=Erreur 404">Message 3</a>
+<a href="?Color=%23ff0000&size=15&message=ça">Message 1</a>
+<a href="?Color=%2339ff00&size=30&message=à l'air">Message 2</a>
+<a href="?Color=%230080ff&size=50&message=de fonctionné">Message 3</a>
+<a href="?Color=black&size=12&message=Sans couleurs">Texte banal</a>
 <?php
     if (empty($_GET)) 
     {
         echo "Erreur 404 : paramètre not found";
     }
-    if (empty($_GET["size"] and $_GET["Color"])) {
-        echo "<div style='font-size: 120px; color: black'>{$_GET["message"]}</div>";
+    if (empty($_GET["size"] and $_GET["Color"])) 
+    {
+        echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
     }
-    else {
+    else 
+    {
         echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
     }
 ?>
