1e304cc52c9f1f12bfed61f29e08844c1a96c92f
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-19 12:04:27 +0100

    Add files via upload

--- td1/exo3.php
+++ td1/exo3.php
@@ -6,19 +6,28 @@
 </head>
 
 
-<form method="GET">
+<body>
+<form method="POST">
     <label for="color">Color : </label>
     <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
     <label for="size">Size : </label>
     <input type="number" value="" name="size" id="size">
     <input type="submit" value="Valider">
 </form>
-<body>
-<a href="?Color=%23ff0000&size=15&message=salut">Message 1</a>
-<a href="?Color=%23ff0000&size=15&message=ça">Message 2</a>
-<a href="?Color=%231ae633&size=60&message=benji">Message 3</a>
-    <?php
-    echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
-    ?>
+<textarea name="message" id="message" placeholder="Écrivez ici"></textarea>
+<?php
+    if (empty($_POST)) 
+    {
+        echo "Erreur 404 : paramètre not found";
+    }
+    if (empty($_POST["size"] and $_POST["Color"])) 
+    {
+        echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
+    }
+    else 
+    {
+        echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
+    }
+?>
 </body>
 </html>
