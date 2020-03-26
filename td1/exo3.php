8ea4444e6848d4b0c710f069c05fa17788f3adb7
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-26 14:16:31 +0100

    commit par save.bat de Jordan Dubu

--- td1/exo3.php
+++ td1/exo3.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>Hello</title>
+	<meta charset="UTF-8">
+	<title>Exo3</title>
 </head>
 
 
 <body>
-<form method="POST">
-    <label for="color">Color : </label>
-    <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
-    <label for="size">Size : </label>
-    <input type="number" value="" name="size" id="size">
-    <input type="submit" value="Valider">
-</form>
-<textarea name="message" id="message" placeholder="Écrivez ici"></textarea>
-<?php
-    if (empty($_POST)) 
-    {
-        echo "Erreur 404 : paramètre not found";
-    }
-    if (empty($_POST["size"] and $_POST["Color"])) 
-    {
-        echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
-    }
-    else 
-    {
-        echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
-    }
-?>
+	<form action="exo3.php" method="POST">
+		<label for="color">Color : </label>
+		<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
+		<label for="size">Size : </label>
+		<input type="number" value="" name="size" id="size">
+		<label for="contenue">Text : </label>
+		<textarea id="message" name="message" placeholder="Écrivez ici">
+		</textarea>
+		<input type="submit" value="Valider">
+	</form>
+	<?php
+	if (empty($_POST)) {
+		echo "Erreur 404 : paramètre not found";
+	}
+	if (empty($_POST["size"] and $_POST["Color"])) {
+		echo "<div style='font-size: 12px; color: black'>{$_POST["message"]}</div>";
+	}
+	else {
+		echo "<div style='font-size: {$_POST["size"]}px; color: {$_POST["Color"]}'>{$_POST["message"]}</div>";
+	}
+	?>
 </body>
 </html>
