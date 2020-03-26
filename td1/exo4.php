6345bdfd0e31865a1a3a5a7606298d5095e6c5c3
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-26 15:58:18 +0100

    Envoye par save.bat de Jordan Dubu

--- td1/exo4.php
+++ td1/exo4.php
@@ -1,13 +1,32 @@
 <?php
 include "header.php";
 ?>
-<?php
-if($i<5){
-    // faire quelque chose si $i est strictement inférieur à 5
-}else{
-    // $i est supérieur ou égal à 5
-}
-?>
+
+
+<a href="?nbligne=2&nbcolonne=2">2 x 2</a>
+<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
+<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
+
+<table border='1'>
+    <tbody>
+       	<?php
+    		for($i=0;$i<$_GET["nbligne"];$i++){
+    			echo"<tr>";
+        			
+        				for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
+        					echo"<td>";
+        					echo"$i-";
+        					echo "$j";
+        					echo"</td>";
+        				}
+        			
+        		echo"</tr>";
+			}
+			?>
+    </tbody>
+</table>
+
+
 <?php
 include "footer.php";
 ?>
