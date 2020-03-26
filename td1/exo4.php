729e03addf989b3960f49438e547310378e30eb0
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-26 16:11:46 +0100

    Envoye par save.bat de Jordan Dubu

--- td1/exo4.php
+++ td1/exo4.php
@@ -1,32 +1,36 @@
-<?php
-include "header.php";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Exo 4</title>
+</head>
 
 
-<a href="?nbligne=2&nbcolonne=2">2 x 2</a>
-<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
-<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
+<body>
+	<a href="?nbligne=2&nbcolonne=2">2 x 2</a>
+	<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
+	<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
 
-<table border='1'>
-    <tbody>
-       	<?php
-    		for($i=0;$i<$_GET["nbligne"];$i++){
-    			echo"<tr>";
-        			
-        				for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
-        					echo"<td>";
-        					echo"$i-";
-        					echo "$j";
-        					echo"</td>";
-        				}
-        			
-        		echo"</tr>";
+	<table border='1'>
+		<tbody>
+			<?php
+			for($i=0;$i<$_GET["nbligne"];$i++){
+				echo"<tr>";
+				
+				for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
+					echo"<td>";
+					echo"$i-";
+					echo "$j";
+					echo"</td>";
+				}
+				
+				echo"</tr>";
 			}
 			?>
-    </tbody>
-</table>
+		</tbody>
+	</table>
 
 
-<?php
-include "footer.php";
-?>
+	<?php
+	include "footer.php";
+	?>
