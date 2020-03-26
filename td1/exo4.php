14cefabf44bc5ab5cf8a3f43808ef648ced1a291
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-03-26 16:22:41 +0100

    Envoye par save.bat de Jordan Dubu

--- td1/exo4.php
+++ td1/exo4.php
@@ -11,19 +11,28 @@
 	<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
 	<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
 
+
+<?php
+if($i%2){
+
+}
+if($j%2){
+
+}
+?>
 	<table border='1'>
 		<tbody>
 			<?php
 			for($i=0;$i<$_GET["nbligne"];$i++){
 				echo"<tr>";
-				
+
 				for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
 					echo"<td>";
 					echo"$i-";
 					echo "$j";
 					echo"</td>";
 				}
-				
+
 				echo"</tr>";
 			}
 			?>
