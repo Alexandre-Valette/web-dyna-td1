187a95631bb5c81c8848eb88993d12b57229cba7
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-04-01 09:24:15 +0200

    Envoye par save.bat de Jordan Dubu

--- td1/exo5.php
+++ td1/exo5.php
@@ -7,7 +7,12 @@
 
 
 <body>
+	<a href="?nbligne=2&nbcolonne=2">2 x 2</a>
+	<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
+	<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
+
 	<?php
 	include "functions.php";
+	createHtmlTable($_GET["nbligne"],$_GET["nbcolonne"]);
 	include "footer.php";
 	?>
--- td1/functions.php
+++ td1/functions.php
@@ -1,36 +1,34 @@
-	<a href="?nbligne=2&nbcolonne=2">2 x 2</a>
-	<a href="?nbligne=5&nbcolonne=5">5 x 5</a>
-	<a href="?nbligne=10&nbcolonne=8">10 x 8</a>
+<?php
+function createHtmlTable($nbligne,$nbcolonne) {
+	echo"<table border='1'>";
+	echo"<tbody>";
+	for($i=0;$i<$_GET["nbligne"];$i++){
+		echo"<tr>";
 
+		for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
 
-	<table border='1'>
-		<tbody>
-			<?php
-			for($i=0;$i<$_GET["nbligne"];$i++){
-				echo"<tr>";
-
-				for ($j=0; $j<$_GET["nbcolonne"]; $j++) { 
-
-					if(($i%2)==0){
-						$Bold = "bold";
-					}
-					else{
-						$Bold = "normal";
-					}
-
-					if(($j%2)==0){
-						$Red = "red";
-					}
-					else{
-						$Red = "black";
-					}
-
-					echo"<td align='center'><p style='font-weight: $Bold; color:$Red;'>$i-$j";
-					echo"</p></td>";
-				}
-
-				echo"</tr>";
+			if(($i%2)==0){
+				$Bold = "bold";
 			}
-			?>
-		</tbody>
-	</table>
+			else{
+				$Bold = "normal";
+			}
+
+			if(($j%2)==0){
+				$Red = "red";
+			}
+			else{
+				$Red = "black";
+			}
+
+			echo"<td align='center'><p style='font-weight: $Bold; color:$Red;'>$i-$j";
+			echo"</p></td>";
+		}
+
+		echo"</tr>";
+	}
+	
+	echo"</tbody>";
+	echo"</table>";
+}
+?>
