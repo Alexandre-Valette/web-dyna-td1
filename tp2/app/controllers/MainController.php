4832895c64c955d7a353bdd6392b8a6b322a6700
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-04-09 16:00:14 +0200

    Envoye par save.bat de Jordan Dubu

--- tp2/app/controllers/MainController.php
+++ tp2/app/controllers/MainController.php
@@ -9,4 +9,14 @@ class MainController extends ControllerBase{
 	public function index(){
 		
 	}
+
+	/**
+	 *@route("send/{message}/{who}","methods"=>["get"])
+	**/
+	public function sendMessage($message,$who='world'){
+		
+		$this->loadView('MainController/sendMessage.html',compact('message','who'));
+
+	}
+
 }
