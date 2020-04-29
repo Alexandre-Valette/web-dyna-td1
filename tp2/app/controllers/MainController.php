7684c67c66da7e70ef2f239b557097c4ae10e122
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-04-29 14:53:04 +0200

    Envoye par save.bat de Jordan Dubu

--- tp2/app/controllers/MainController.php
+++ tp2/app/controllers/MainController.php
@@ -7,7 +7,7 @@ class MainController extends ControllerBase{
      * @get("_default","name"=>"Home")
      **/
 	public function index(){
-	    
+	    $this->loadView("MainController/index.html");
 	}
 
 	/**
